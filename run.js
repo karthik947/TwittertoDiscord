0a6e6bdc85ab3f6d97c2a6913cf55fc215b1b0cf
Karthik D <karthik947@gmail.com>
2020-07-02 15:51:54 +0530

    Update run.js

--- run.js
+++ run.js
@@ -12,7 +12,7 @@ var THandlers=[
     {
         name:'Karthik',
         url:"https://twitter.com/Karthikdk72?lang=en",
-        webhook:"https://discordapp.com/api/webhooks/628116875481579520/V-UoVZPPnSmkWHAO99Pbn9IKSWhlMuLQlwqAKPOwwrwGCrEI2gbnwCzW05j0MChWNGWz",
+        webhook:"",
         avatar_url:"https://c8.alamy.com/comp/REBBXT/manbrunettehairwighaircutraglanorangehairdresserfashionavatardummypersonimageportraithairstyleprofessionalphotocharacterprofilesetvectoriconillustrationisolatedcollectiondesignelementgraphicsignblacksimple-vector-vectors-REBBXT.jpg",
         keywords:"*",
     },
